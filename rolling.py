b56070b4dfbcd2d0e09070afc30d4b8895fd4003
goescat <goescat1024@gmail.com>
2019-08-17 06:31:13 +0800

    Update rolling.py
    1. Change to implicitly wait
    2. Click pushpad deny button to handle alert

--- rolling.py
+++ rolling.py
@@ -9,15 +9,14 @@ from selenium.webdriver.support import expected_conditions as EC
 
 def auto_roll():
 
-    browser = webdriver.Chrome("Your Webdriver Path") # Your webdriver path
+    browser = webdriver.Chrome("/chromedriver") # Your webdriver path
     browser.get("https://freebitco.in/")
+    browser.implicitly_wait(30)
 
-    #wait page and alert loading
-    wait = WebDriverWait(browser, 10)
-    element = wait.until(EC.element_to_be_clickable((By.XPATH,"(//div[@class='allow_button pushpad_allow_button'])[1]")))
     # Try to close allow alert
     try:
-        allow_btn = "(//div[@class='allow_button pushpad_allow_button'])[1]"
+        time.sleep(10)
+        allow_btn = "(//div[@class='pushpad_deny_button'])[1]"
         browser.find_element_by_xpath(allow_btn).click()
     except:
         pass
@@ -29,28 +28,14 @@ def auto_roll():
     elem = browser.find_element_by_id("login_form_password")
     elem.send_keys("Your Password") # Your password
 
-    # Try to close banner message
-    try:
-        browser.find_element_by_id("cc_banner").click()
-    except:
-        pass
-
     browser.find_element_by_id("login_button").click()
-    time.sleep(3) # Wait for page loading
 
     #wait page and alert loading
-    wait = WebDriverWait(browser, 10)
-    element = wait.until(EC.element_to_be_clickable((By.XPATH,"(//div[@class='allow_button pushpad_allow_button'])[1]")))
     # Try to close allow alert
     try:
-        allow_btn = "(//div[@class='allow_button pushpad_allow_button'])[1]"
+        time.sleep(10)
+        allow_btn = "(//div[@class='pushpad_deny_button'])[1]"
         browser.find_element_by_xpath(allow_btn).click()
-    except:
-        pass
-
-    # Try to close message
-    try:
-        browser.find_element_by_class_name("cc_btn").click()
     except:
         pass
 
